ecc9c81117489e1ddc667c108de950a7c48a6533
Tanzhor_RIK <tanzhor4@gmail.com>
2023-04-12 20:02:25 +0700

    add function test_in_date_time

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,5 @@
 from src import utils
+import datetime
 
 
 def test_data_sorted(test_data_filter):
@@ -7,7 +8,8 @@ def test_data_sorted(test_data_filter):
 
     print("\n" + "-------------------" + "\n")
 
-    print(*[x["date"] for x in utils.data_sorted(test_data_filter)][:5], sep="\n")
+    print(*[x["date"] for x in utils.data_sorted(test_data_filter)][:5],
+          sep="\n")
 
     assert test_data_filter[:5] != utils.data_sorted(test_data_filter)[:5]
 
@@ -25,3 +27,19 @@ def test_data_filter(test_data):
 
     assert all(
         x["state"] == "EXECUTED" for x in utils.data_filter(test_data)) == True
+
+
+def test_in_date_time(test_data_strings_date):
+    print("\n\nВо всех данных статус тип str:")
+    print(all(type(x) == str for x in test_data_strings_date))
+
+    assert all(type(x) == str for x in test_data_strings_date) == True
+
+    print("\n" + "-------------------" + "\n")
+
+    print("Во всех данных статус тип datetime.datetime:")
+    print(all(type(utils.in_date_time(x)) == datetime.datetime for x in
+              test_data_strings_date))
+
+    assert all(type(utils.in_date_time(x)) == datetime.datetime for x in
+               test_data_strings_date) == True
